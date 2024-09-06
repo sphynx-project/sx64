46cdf3ff764ae735ddd887d291306e3ef64e633f
Kevin Alavik <kevin@alavik.se>
2024-09-06 19:02:45 +0200

    feat:  New test

--- firmware/build.sh
+++ firmware/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3 ../tools/asm.py $(pwd)/../firmware/bios.asm ../bios.bin
-python3 ../tools/asm.py $(pwd)/../firmware/boot.asm ../boot.bin
+python3 ../tools/asm.py bios.asm ../bios.bin
+python3 ../tools/asm.py boot.asm ../boot.bin
